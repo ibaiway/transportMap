68f4c164c2d03fe88f79c4e1892c857e9cd2c5f4
ibaialberdi <ibaialberdi@gmail.com>
2025-05-10 15:36:25 +0200

    addded new gtfs data sources

--- src/config/gtfs-config.ts
+++ src/config/gtfs-config.ts
@@ -2,12 +2,26 @@ import { Config } from "gtfs"
 
 const gtfsConfig: Config = {
   sqlitePath: "./db/gtfs.db",
+
+  ignoreDuplicates: false,
   agencies: [
     {
       url: "https://opendata.euskadi.eus/transport/moveuskadi/dbus/gtfs_dbus.zip",
+      prefix: "dbus",
       realtimeTripUpdates: {
         url: "https://opendata.euskadi.eus/transport/moveuskadi/dbus/gtfsrt_dbus_trip_updates.pb",
       },
+    },
+    {
+      url: "https://opendata.euskadi.eus/transport/moveuskadi/lurraldebus/ekialdebus/gtfs_ekialdebus.zip",
+      prefix: "ekialdebus",
+      realtimeTripUpdates: {
+        url: "https://opendata.euskadi.eus/transport/moveuskadi/lurraldebus/ekialdebus/gtfsrt_ekialdebus_vehicle_positions.pb",
+      },
+    },
+    {
+      url: "https://opendata.euskadi.eus/transport/moveuskadi/euskotren/gtfs_euskotren.zip",
+      prefix: "euskotren",
     },
   ],
 }
